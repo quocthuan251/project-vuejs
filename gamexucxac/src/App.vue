adbc21b92fd40a65f99872ca8efc72b9bd0acaf8
Trần Quốc Thuận <quocthuan251@gmail.com>
2020-04-20 21:06:01 +0700

    game dice - code vuejs
    game xuc xac code vuejs

---
+++ gamexucxac/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+         <div class="wrapper clearfix">
+            <Players :scorePlayer="scorePlayer"
+            :activePlayer="activePlayer"
+            :currenScore="currenScore"/>
+            
+           <Controls
+           @handleNewGame="handleNewGame"
+           />
+            <Dices :dice="dice"/>
+       
+        </div>
+  </div>
+</template>
+
+<script>
+import Players from "./components/Players"
+import Controls from "./components/Controls"
+import Dices from "./components/Dices"
+import PopupRule from "./components/PopupRule"
+export default {
+  name: 'app',
+  data () {
+    return {
+      activePlayer: 0,
+      scorePlayer: [12,11],
+      currenScore: 30,
+      isPlaying: false,
+      dice: [1,1],
+    }
+  },
+  components:{
+    Players,
+    Controls,
+    Dices,
+    PopupRule,
+  },
+  methods:{
+    handleNewGame(){
+       console.log('jejelfjla adfjal');
+       this.isPlaying=true;
+       this.activePlayer=0;
+       this.scorePlayer=[0,0]
+       this.currenScore=0
+       this.dice=[1,1]
+       
+    }
+  }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+}
+
+.clearfix::after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+body {
+    background-image: linear-gradient(rgba(62, 20, 20, 0.4), rgba(62, 20, 20, 0.4)), url('/dist/back.jpg');
+    background-size: cover;
+    background-position: center;
+    font-family: Lato;
+    font-weight: 300;
+    position: relative;
+    height: 100vh;
+    color: #555;
+}
+
+.wrapper {
+    width: 1000px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+    overflow: hidden;
+}
+
+
+
+
+
+</style>
---
+++ gamexucxac/src/components/PopupRule.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper-popup">
+    <h3>luật chơi</h3>
+    <p>luật abc xyz...</p>
+    <button>đã hiểu</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'popup',
+    data(){
+        return{
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
